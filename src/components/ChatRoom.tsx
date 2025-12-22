831b741e7222aa2c842987f595e689c8986b9342
“BellWin98” <jongseunghan@naver.com>
2025-12-22 22:17:18 +0900

    내부스크롤 안되는 버그 조치

--- src/components/ChatRoom.tsx
+++ src/components/ChatRoom.tsx
@@ -281,7 +281,7 @@ export const ChatRoom = ({ currentWorkoutRoom }) => {
             onChange={(e) => setInput(e.target.value)}
             onKeyDown={handleKeyDown}
             placeholder="메시지를 입력하세요"
-            className="flex-2 max-h-24 min-h-[20px] resize-none leading-relaxed py-1"
+            className="flex-2 max-h-24 min-h-[20px] resize-none leading-relaxed py-1 overflow-y-auto"
           />
           <input type="file" accept="image/*" onChange={handleFileChange} className="hidden" id="chat-image-upload" />
           {/* <label htmlFor="chat-image-upload">
